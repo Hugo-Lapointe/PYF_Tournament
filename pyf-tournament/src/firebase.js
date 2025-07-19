f6e052e1c590759b8246294aea62471646d4218e
Rapid <hlapointe8191@gmail.com>
2025-07-19 16:59:58 -0400

    PROGRESS configuration done now adding tables and admin panels

---
+++ pyf-tournament/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBwYXmgV-Ljh0D9dpjohRdJ7q083HjZ288",
+  authDomain: "tournamentapp-d4671.firebaseapp.com",
+  projectId: "tournamentapp-d4671",
+  storageBucket: "tournamentapp-d4671.firebasestorage.app",
+  messagingSenderId: "1064386528497",
+  appId: "1:1064386528497:web:f66686ed7305631d419550",
+  measurementId: "G-WYE63DKD05"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
